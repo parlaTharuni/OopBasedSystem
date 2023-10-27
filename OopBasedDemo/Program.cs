dc8f2755d7fa499481f27e7459b97461d5cca14d
parlaTharuni <tharuniparla@gmail.com>
2023-10-27 13:55:34 +0530

    Add project files.

---
+++ OopBasedDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OopBasedDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RainbowSchool rainbowSchool = new RainbowSchool();
+
+            // Filling up the lists with data
+            rainbowSchool.AddStudent("Student1", "ClassA");
+            rainbowSchool.AddStudent("Student2", "ClassB");
+
+            rainbowSchool.AddTeacher("Teacher1", "ClassA");
+            rainbowSchool.AddTeacher("Teacher2", "ClassB");
+
+            rainbowSchool.AddSubject("Science", "science01", rainbowSchool.Teachers[0]);
+            rainbowSchool.AddSubject("English", "english-12T", rainbowSchool.Teachers[1]);
+
+            // Display students in a class
+            List<Student> classAStudents = rainbowSchool.GetStudentsInClass("ClassA");
+            Console.WriteLine("Students in ClassA:");
+            foreach (var student in classAStudents)
+            {
+                Console.WriteLine($"- {student.Name}");
+            }
+            Console.ReadLine();
+            // Display subjects taught by a teacher
+            List<Subject> teacher2Subjects = rainbowSchool.GetSubjectsTaughtByTeacher("Teacher2");
+            Console.WriteLine("Subjects taught by Teacher2:");
+            foreach (var subject in teacher2Subjects)
+            {
+                Console.WriteLine($"- {subject.Name} (Code: {subject.SubjectCode})");
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
+
---
+++ OopBasedDemo/Rainbow.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopBasedDemo
+{
+    
+        class Student
+        {
+            public string Name { get; set; }
+            public string ClassSection { get; set; }
+        }
+
+        class Teacher
+        {
+            public string Name { get; set; }
+            public string ClassSection { get; set; }
+        }
+
+        class Subject
+        {
+            public string Name { get; set; }
+            public string SubjectCode { get; set; }
+            public Teacher Teacher { get; set; }
+        }
+
+        class RainbowSchool
+        {
+            public List<Student> Students { get; } = new List<Student>();
+            public List<Teacher> Teachers { get; } = new List<Teacher>();
+            public List<Subject> Subjects { get; } = new List<Subject>();
+
+            public void AddStudent(string name, string classSection)
+            {
+                Students.Add(new Student { Name = name, ClassSection = classSection });
+            }
+
+            public void AddTeacher(string name, string classSection)
+            {
+                Teachers.Add(new Teacher { Name = name, ClassSection = classSection });
+            }
+
+            public void AddSubject(string name, string subjectCode, Teacher teacher)
+            {
+                Subjects.Add(new Subject { Name = name, SubjectCode = subjectCode, Teacher = teacher });
+            }
+
+            public List<Student> GetStudentsInClass(string classSection)
+            {
+                return Students.FindAll(student => student.ClassSection == classSection);
+            }
+
+            public List<Subject> GetSubjectsTaughtByTeacher(string teacherName)
+            {
+                Teacher teacher = Teachers.Find(t => t.Name == teacherName);
+                if (teacher != null)
+                {
+                    return Subjects.FindAll(subject => subject.Teacher == teacher);
+                }
+                return new List<Subject>();
+            }
+        }
+
+    }
+
